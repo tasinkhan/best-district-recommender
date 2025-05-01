36c6e3dd1779419eebd4da54f92302778386883b
Tasin <tasintsn@gmail.com>
2025-05-01 17:10:54 +0600

    Fix DJANGO_SETTINGS_MODULE and Celery app name to match project structure

--- district_recommender/celery.py
+++ district_recommender/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "traveladvisor.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "district_recommender.settings")
 
-app = Celery("traveladvisor")
+app = Celery("district_recommender")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
